ebfbd8bdb082488727cc3cf74108ad499758429f
lhxyq <2446059046@qq.com>
2017-06-22 18:23:02 +0800

    单点登录(SSO)初始化

---
+++ system001/src/main/java/com/lh/filter/LoginFilter.java
@@ -0,0 +1,33 @@
+package com.lh.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by LH 2446059046@qq.com on 2017/6/22.
+ */
+public class LoginFilter  implements Filter{
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session =  request.getSession();
+
+        boolean isLogin = session.getAttribute("isLogin") != null && (Boolean) session.getAttribute("isLogin");
+
+        if (isLogin)
+            filterChain.doFilter(servletRequest, servletResponse);
+        else
+            response.sendRedirect("http://localhost:8080/user/");
+    }
+
+    public void destroy() {
+
+    }
+}
